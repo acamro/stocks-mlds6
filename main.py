b56fa7d74329588816b8dff1183f9ec2932841dc
Andrés Rodríguez <acamro@users.noreply.github.com>
2024-12-12 23:54:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ model = joblib.load("model.joblib")
 # Reemplace esto con su implementación:
 @app.post("/predict")
 async def predict(data: ApiInput) -> ApiOutput:
-    model = joblib.load("model.joblib")
     preds = model.predict([data.features]).flatten().tolist() # generamos la predicción
     prediction = ApiOutput(forecast=preds[0]) # estructuramos la salida del API.
     return prediction
